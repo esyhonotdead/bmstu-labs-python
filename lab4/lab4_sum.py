2bad75ca39e1a50032700a863710cd61a483eff4
esyhonotdead <esyho.main@gmail.com>
2024-10-03 10:05:28 +0300

    smert jopi

--- lab4/lab4_sum.py
+++ lab4/lab4_sum.py
@@ -1,35 +1,42 @@
 """
 Пысларь Никита, ИУ7-11Б
 Лабораторная работа #4, Вариант - 24
-Составить программу вычисления суммы ряда (по варианту) с точностью до члена ряда ε.
-Необходимо вывести таблицу промежуточных вычислений с заданным шагом и результат вычисления.
+Задание:
+1) Составить программу вычисления суммы ряда (по варианту) с точностью до члена ряда ε.
+2) Необходимо вывести таблицу промежуточных вычислений с заданным шагом и результат вычисления.
 """
 
 # Нормальные таблицы
 from prettytable import PrettyTable
 
 
-def sum_series(x, epsilon, step):
+def sum_series(x, epsilon, step, max_n):
     # Инициализация переменных
     n = 1
     sum_result = 1
     term = 1 / (n**x)
     my_table = PrettyTable(["# Итерации", "t", "y"])
-    my_table.add_row([n, round(term, 3), round(sum_result, 3)])
+    my_table.add_row([n, term, sum_result])
 
     # Пока текущий член больше или равен epsilon, продолжаем
-    while abs(term) >= epsilon:
+    while abs(term) >= epsilon and n <= max_n:
         term = 1 / (n**x)
         sum_result += term
         if n % step == 0:
-            my_table.add_row([n, round(term, 5), round(sum_result, 5)])
+            term_temp = float(f"{term:.5g}")
+            sum_result_temp = float(f"{sum_result:.5g}")
+            my_table.add_row([n, term_temp, sum_result_temp])
         n += 1
     print(my_table)
-    print(f"Сумма бесконечного ряда {round(sum_result, 5)}, вычислена за {n} итераций.")
+    if abs(term) > epsilon:
+        print(f"Сумма не вычеслена на {n-1} операций.")
+    else:
+        print(f"Сумма бесконечного ряда {sum_result:.5g}, вычислена за {n-1} итераций.")
 
 
 # Ввод данных:
+max_n = int(input("Введите максимальное число итераций: "))
 x = int(input("Введите степень ряда: "))
-step = int(input("введите шаг печати: "))
+step = int(input("Ведите шаг печати: "))
 epsilon = float(input("Введите точность: "))
-sum_series(x, epsilon, step)
+sum_series(x, epsilon, step, max_n)
