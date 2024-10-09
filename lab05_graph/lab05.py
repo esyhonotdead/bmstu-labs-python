7da4a06d1be6e79375db3779822a39103427e472
esyhonotdead <esyho.main@gmail.com>
2024-10-10 00:51:38 +0300

    победа над 5 лабой

--- lab05_graph/lab05.py
+++ lab05_graph/lab05.py
@@ -4,16 +4,17 @@
 Задание:
 1) Написать программу для вывода значения функции в таблицу (вводные данные: начальное и конечное значение аругмента, шаг разбиения аргумента)
 2) Построить график функции y3
+3) Найти y3 min и xmin при котором оно достигается
 """
 
 import math
 
 # ввод значений
 while True:
-    x_start = int(input("Введите начальное значение x: "))
-    x_end = int(input("Введите конечное значение x: "))
-    step = int(input("Введите шаг: "))
-    if x_start <= 0 and x_end < 0 and step < 0:
+    x_start = float(input("Введите начальное значение x: "))
+    x_end = float(input("Введите конечное значение x: "))
+    step = float(input("Введите шаг: "))
+    if x_start <= 0 and x_end < 0 or step < 0:
         print("Пожалуйста, введите корректные значения.")
         continue
     else:
@@ -25,8 +26,13 @@ print("-" * len(header))
 print(header)
 print("-" * len(header))
 
+x_count = int((x_end - x_start) / step)
+
 # данные таблицы
-for x in range(x_start, x_end + 1, step):
+
+x_count = int((x_end - x_start) / step)
+for i in range(x_count + 1):
+    x = i * step + x_start
     y1 = math.pow(x, 2) - 4 * x  # Функция #1
     y2 = math.pow(x, 3) - 3 * math.pow(x, 2) + 1  # Функция #2
     y3 = (y1 + y2) / 10  # Функция #3
@@ -48,20 +54,24 @@ y3_max = (
     math.pow(x_end, 2) - 4 * x_end + math.pow(x_end, 3) - 3 * math.pow(x_end, 2) + 1
 ) / 10
 
+# доп. задание
+print(" ")
 print(f"Минимальное значение y3 = {y3_min}, при x = {x_start}.")
 print(f"Максимальное значение y3 = {y3_max}, при x = {x_end}.")
 print(" ")
 
 # засечеки
 while True:
+    width = int(input("Введите ширину графика: "))  # ширина графика
     num_ticks = int(input("Введите количество засечек на оси ординат (от 4 до 8): "))
-    if 4 <= num_ticks <= 8:
+    if 4 <= num_ticks <= 8 and width > 0:
         break
     else:
-        print("Пожалуйста, введите значение от 4 до 8.")
+        print(
+            "Пожалуйста, введите значение от 4 до 8 или положительное значение ширины."
+        )
 
 # график
-width = 80  # ширина графика
 scale_y = (y3_max - y3_min) / (width - 1)  # масштабирование по оси Y
 
 # линейка для оси ординат
@@ -73,7 +83,8 @@ for i in range(num_ticks):
 print()
 
 # построение графика
-for x in range(x_start, x_end + 1, step):
+for i in range(x_count + 1):
+    x = i * step + x_start
     # x слева
     print(f"{x:>8} |", end="")
 
